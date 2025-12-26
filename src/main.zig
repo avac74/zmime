2e2b464c48ac1595e2a96be9c772c4c332f37188
Andre Cruz <avac.fp@gmail.com>
2025-12-26 22:28:31 +0100

    make args handling cross-platform

--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,20 @@ const CliError = error{
 };
 
 pub fn main() !void {
-    if (std.os.argv.len != 2) {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // Cross-platform argument handling
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len != 2) {
         std.log.err("usage: zmime <file name>", .{});
         return CliError.MissingFileName;
     }
 
-    const file_name: [:0]const u8 = std.mem.span(std.os.argv[1]);
+    const file_name = args[1];
 
     const result = try zmime.detectFileType(file_name);
     std.log.info("File type is {s}", .{@tagName(result)});
